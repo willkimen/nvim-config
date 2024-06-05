05a7625d7acb0b7dfa4dcb54f3164295d02b1c78
Willkimen <willkimen.dev@proton.me>
2024-06-05 01:16:27 -0300

    refactor: Muda as teclas para selecionar o autocomplete

--- init.lua
+++ init.lua
@@ -513,9 +513,9 @@ require('lazy').setup({
         -- No, but seriously. Please read `:help ins-completion`, it is really good!
         mapping = cmp.mapping.preset.insert {
           -- Select the [n]ext item
-          ['<C-n>'] = cmp.mapping.select_next_item(),
+          --['<C-n>'] = cmp.mapping.select_next_item(),
           -- Select the [p]revious item
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
+          --['<C-p>'] = cmp.mapping.select_prev_item(),
 
           -- Scroll the documentation window [b]ack / [f]orward
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -524,13 +524,14 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          --['<C-y>'] = cmp.mapping.confirm { select = true },
 
-          -- If you prefer more traditional completion keymaps,
-          -- you can uncomment the following lines
-          --['<CR>'] = cmp.mapping.confirm { select = true },
-          --['<Tab>'] = cmp.mapping.select_next_item(),
-          --['<S-Tab>'] = cmp.mapping.select_prev_item(),
+          -- Aperta enter para escolher o autocomplete, antes era ['<C-y>'], acima.
+          ['<CR>'] = cmp.mapping.confirm { select = true },
+          -- Com tab você navega para o próximo autocomplete, antes era ['<C-n>'], acima.
+          ['<Tab>'] = cmp.mapping.select_next_item(),
+          -- Seleciona o anterior usando shift tab, antes era ['<C-p>'], acima.
+          ['<S-Tab>'] = cmp.mapping.select_prev_item(),
 
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display
