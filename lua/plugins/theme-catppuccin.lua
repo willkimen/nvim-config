8550cc099820ae7469f24ec82e5821a3df777952
Willkimen <willkimen.dev@proton.me>
2024-06-06 15:04:44 -0300

    feat: Instala o tema para catppuccin

---
+++ lua/plugins/theme-catppuccin.lua
@@ -0,0 +1,12 @@
+return {
+    "catppuccin/nvim",
+    as = "catppuccin",
+    config = function()
+      require("catppuccin").setup({
+        -- Configurações opcionais
+        flavour = "mocha", -- Opções: latte, frappe, macchiato, mocha
+      })
+      vim.cmd.colorscheme("catppuccin")
+    end
+}
+
