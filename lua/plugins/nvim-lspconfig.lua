12f07b7b900d88b93f8a42a901869e7dc456597b
Willkimen <willkimen.dev@proton.me>
2024-06-05 23:29:14 -0300

    docs: Traduz partes dos comentários

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -56,46 +56,47 @@ return { -- ############### LSP Configuration & Plugins ###############
             vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
           end
 
-          -- Jump to the definition of the word under your cursor.
-          --  This is where a variable was first declared, or where a function is defined, etc.
-          --  To jump back, press <C-t>.
+          -- Pula para a definição da palavra sob o seu cursor.
+          -- Isso é onde uma variável foi primeiro declarada, ou onde uma função é definida, etc.
+          -- Para voltar, pressione <C-t>.
           map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
 
-          -- Find references for the word under your cursor.
+          -- Encontra referências para a palavra sob o seu cursor.
           map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
 
-          -- Jump to the implementation of the word under your cursor.
-          --  Useful when your language has ways of declaring types without an actual implementation.
+          -- Pula para a implementação da palavra sob o seu cursor.
+          -- Útil quando sua linguagem tem maneiras de declarar tipos sem uma implementação real.
           map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
 
-          -- Jump to the type of the word under your cursor.
-          --  Useful when you're not sure what type a variable is and you want to see
-          --  the definition of its *type*, not where it was *defined*.
+          -- Pula para o tipo da palavra sob o seu cursor.
+          -- Útil quando você não tem certeza de qual tipo uma variável é e você quer ver
+          -- a definição de seu *tipo*, não onde ela foi *definida*.
           map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
 
-          -- Fuzzy find all the symbols in your current document.
-          --  Symbols are things like variables, functions, types, etc.
+          -- Faz uma busca fuzzy por todos os símbolos no seu documento atual.
+          -- Símbolos são coisas como variáveis, funções, tipos, etc.
           map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
 
-          -- Fuzzy find all the symbols in your current workspace.
-          --  Similar to document symbols, except searches over your entire project.
+          -- Faz uma busca fuzzy por todos os símbolos no seu workspace atual.
+          -- Similar a document symbols, exceto que busca em todo o seu projeto.
           map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
-          -- Rename the variable under your cursor.
-          --  Most Language Servers support renaming across files, etc.
+          -- Renomeia a variável sob o seu cursor.
+          -- A maioria dos Language Servers suporta renomeação entre arquivos, etc.
           map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
 
-          -- Execute a code action, usually your cursor needs to be on top of an error
-          -- or a suggestion from your LSP for this to activate.
+          -- Executa uma ação de código, geralmente seu cursor precisa estar sobre um erro
+          -- ou uma sugestão do seu LSP para isso ativar.
           map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
-          -- Opens a popup that displays documentation about the word under your cursor
-          --  See `:help K` for why this keymap.
+          -- Abre um popup que exibe documentação sobre a palavra sob o seu cursor.
+          -- Veja `:help K` para entender por que essa keymap existe.
           map('K', vim.lsp.buf.hover, 'Hover Documentation')
 
-          -- WARN: This is not Goto Definition, this is Goto Declaration.
-          --  For example, in C this would take you to the header.
+          -- ATENÇÃO: Isso não é Goto Definition, isso é Goto Declaration.
+          -- Por exemplo, em C isso te levaria para o header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+
 
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
