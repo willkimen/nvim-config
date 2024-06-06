163edde229b4e2345a9e1ec433daceda115223b4
Willkimen <willkimen.dev@proton.me>
2024-06-06 12:05:49 -0300

    Retira o lazy.lock do gitignore, traduz para o portugues partes dos comentarios

--- init.lua
+++ init.lua
@@ -35,8 +35,8 @@ require('lazy').setup({
   require 'plugins.gitsigns', -- adds gitsigns recommend keymaps
 }, {
   ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+  -- Se você estiver usando uma Nerd Font: defina icons como uma tabela vazia, que usará
+  -- os ícones Nerd Font padrão definidos pelo lazy.nvim. Caso contrário, defina uma tabela com ícones Unicode.
     icons = vim.g.have_nerd_font and {} or {
       cmd = '⌘',
       config = '🛠',
