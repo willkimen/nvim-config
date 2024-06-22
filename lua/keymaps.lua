cdc7e6bbfca6e7e55ba7c657de26ed3a71b43289
Willkimen <willkimen.dev@proton.me>
2024-06-22 17:58:14 -0300

    feat: Insere novas teclas de atalhos
    - Insere a tecla de atalho tab para mudar de aba
    - Insere teclas de atalho como ctrl s e ctrl S para salvar o buffer

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,13 +2,21 @@
 --  See `:help vim.keymap.set()`
 --  See `:help wincmd` para obter uma lista de todos os comandos da janela
 
+-- Pressionar Esc no modo normal limpa o destaque da pesquisa.
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
--- Diagnostic keymaps
+-- Atalhos de diagnóstico
+-- [d -> Vai para a mensagem de diagnóstico anterior.
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+-- ]d -> Vai para a próxima mensagem de diagnóstico.
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+-- Mostra mensagens de erro de diagnóstico em um popup.
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+-- Abre a lista de quickfix com mensagens de diagnóstico.
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+
+
 
 -- Saia do modo terminal no terminal integrado com um atalho que é um pouco mais fácil para as pessoas descobrirem. 
 -- Caso contrário, você normalmente precisa pressionar <C-\><C-n>, o que
@@ -16,7 +24,12 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 
 -- NOTE: Isso não funcionará em todos os emuladores de terminal/tmux/etc. Experimente seu próprio mapeamento
 -- ou apenas use <C-\><C-n> para sair do modo terminal
+
+--Pressionar Esc duas vezes no modo terminal muda para o modo normal.
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+
+
 
 -- TIP: Desativa as teclas de seta no modo normal
 vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
@@ -24,18 +37,35 @@ vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
+
+
 -- Atalhos de teclado para facilitar a navegação dividida.
 --  Use CTRL+<hjkl> para alternar entre janelas
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' }) -- ctrl h
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' }) -- ctrl l
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' }) -- ctrl j
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' }) -- ctrl k
 
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+-- [[ ----------------  Meus atalhos ------------------------ ]]
 
 -- O caractere ; pode ser usado agora para acessar modo command.
 vim.keymap.set('n', ';', ':', { desc = 'CMD enter command mode' })
+
 -- Os caracteres j+k para sair do modo insert para modo normal.
 vim.keymap.set('i', 'jk', '<ESC>')
+
 -- Agora ctrl z está desativado, para não sair da sessão do neovim sem querer.
 vim.keymap.set('n', '<C-z>', '<Nop>', { noremap = true, silent = true })
+
+
+-- Navegar para a próxima aba/tab usando a tecla Tab
+vim.api.nvim_set_keymap('n', '<Tab>', ':tabnext<CR>', { noremap = true, silent = true })
+
+-- Salvar o buffer atual com Ctrl+s
+vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-s>', '<C-o>:w<CR>', { noremap = true, silent = true })
+
+-- Salvar todos os buffers com Ctrl+Shift+s
+vim.api.nvim_set_keymap('n', '<C-S-s>', ':wa<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-S-s>', '<C-o>:wa<CR>', { noremap = true, silent = true })
 
