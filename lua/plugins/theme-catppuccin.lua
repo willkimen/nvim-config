c13dbf95a767b7e5021ba4c35a21e4c746cec52d
Willkimen <willkimen.dev@proton.me>
2025-05-27 22:08:53 -0300

    feat: Add transparencia para o theme catppucin

--- lua/plugins/theme-catppuccin.lua
+++ lua/plugins/theme-catppuccin.lua
@@ -5,6 +5,28 @@ return {
     require('catppuccin').setup {
       -- Configurações opcionais
       flavour = 'mocha', -- Opções: latte, frappe, macchiato, mocha
+      transparent_background = true,
+      styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = { 'italic' }, -- Change the style of comments
+        conditionals = { 'italic' },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+      },
+      integrations = {
+        gitsigns = true,
+        mini = {
+          enabled = true,
+          indentscope_color = '',
+        },
+      },
     }
     vim.cmd.colorscheme 'catppuccin'
   end,
