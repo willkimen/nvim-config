59b150b4ed0b1fdb951ec615a209518b05efb05e
lab <lab@example.com>
2025-10-26 20:52:29 -0300

    feat: Add the option to generate the project name automatically

--- scripts/create_go_project.sh
+++ scripts/create_go_project.sh
@@ -2,8 +2,16 @@
 
 set -e
 
-mkdir golab
-cd golab
-go mod init golab
-mkdir -p cmd/golab internal configs builds
-touch cmd/golab/main.go
+# Verifica se foi passado um argumento
+if [ -z "$1" ]; then
+  echo "Uso: $0 <project-name>"
+  exit 1
+fi
+
+PROJECT_NAME=$1
+
+mkdir "$PROJECT_NAME"
+cd "$PROJECT_NAME"
+go mod init "$PROJECT_NAME"
+mkdir -p cmd/"$PROJECT_NAME" internal configs builds pkg scripts api
+touch cmd/"$PROJECT_NAME"/main.go
--- scripts/create_py_project.sh
+++ scripts/create_py_project.sh
@@ -2,8 +2,16 @@
 
 set -e
 
-uv init pylab
-cd pylab
+# Verifica se foi passado um argumento
+if [ -z "$1" ]; then
+  echo "Usage: $0 <project-name>"
+  exit 1
+fi
+
+PROJECT_NAME=$1
+
+uv init "$PROJECT_NAME"
+cd "$PROJECT_NAME"
 uv add pyright --dev
 
 pyright_config='
@@ -13,3 +21,4 @@ venvPath="."
 venv=".venv"
 '
 echo "$pyright_config" >> pyproject.toml
+
