23c27f60e1cb63f9fd6c9b9a1342ee13e1aaed30
lab <lab@example.com>
2025-11-30 12:09:34 -0300

    feat: remove pyright and add ty lsp

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -196,7 +196,7 @@ return {
     local servers = {
       -- clangd = {},
       gopls = {},
-      pyright = {},
+      ty = {},
       -- rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
