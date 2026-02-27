76a25a5690bb66b72196cfa6fff917cfd197b916
Willkimen <willkimen.dev@proton.me>
2026-02-27 20:45:57 -0300

    refactor: mantem o treesitter atualizado com kickstart nvim
    No repo do kickstart está como require('nvim-treesitter').install(parsers), mas o quando eu
    iniciava o neovim, gerava o erro: attempt to call field 'install' (a nil value).
    Então troquei pela forma abaixo e funcinou. Foi o chatgpt quem indicou e funcinou.

--- lua/plugins/treesitter/nvim-treesitter.lua
+++ lua/plugins/treesitter/nvim-treesitter.lua
@@ -1,20 +1,41 @@
-return { -- Highlight, edit, and navigate code
-  'nvim-treesitter/nvim-treesitter',
-  branch = 'master',
-  build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-  -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-  opts = {
-    ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'python' },
-    -- Autoinstall languages that are not installed
-    auto_install = true,
-    highlight = {
-      enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
-    },
-    indent = { enable = true, disable = { 'ruby' } },
-  },
-}
+return  { -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    build = ':TSUpdate',
+    branch = 'main',
+    -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
+    config = function()
+      local parsers = {
+      'bash', 'c', 'diff',
+      'html', 'lua', 'luadoc',
+      'markdown', 'markdown_inline',
+      'query', 'vim', 'vimdoc',
+      'go', 'python', 'javascript', 'typescript'
+    }
+    -- No repo do kickstart estava como require('nvim-treesitter').install(parsers), mas o quando eu
+    -- iniciava o neovim, gerava o erro: attempt to call field 'install' (a nil value)
+    -- Então troquei pela forma abaixo e funcinou. Foi o chatgpt quem indicou
+      require('nvim-treesitter.install').ensure_installed(parsers)
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function(args)
+          local buf, filetype = args.buf, args.match
+
+          local language = vim.treesitter.language.get_lang(filetype)
+          if not language then return end
+
+          -- check if parser exists and load it
+          if not vim.treesitter.language.add(language) then return end
+          -- enables syntax highlighting and other treesitter features
+          vim.treesitter.start(buf, language)
+
+          -- enables treesitter based folds
+          -- for more info on folds see `:help folds`
+          -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+          -- vim.wo.foldmethod = 'expr'
+
+          -- enables treesitter based indentation
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+    end,
+  }
