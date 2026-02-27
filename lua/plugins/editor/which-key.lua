20d2ba1e291e1dccfd8d25af56d08fe0a9d7c78e
Willkimen <willkimen.dev@proton.me>
2026-02-27 19:00:45 -0300

    feat: Add 'gr' group to which-keys
    Adds 'gr' to which-keys documentation, so users can see that LSP actions are grouped after 'gr' key binds.
    
    Co-authored-by: thiago-negri <evohunz@gmail.com>

--- lua/plugins/editor/which-key.lua
+++ lua/plugins/editor/which-key.lua
@@ -14,6 +14,7 @@ return { -- Useful plugin to show you pending keybinds.
       { '<leader>s', group = '[S]earch', mode = { 'n', 'v' } },
       { '<leader>t', group = '[T]oggle' },
       { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+      { 'gr', group = 'LSP Actions', mode = { 'n' } },
     },
   },
 }
