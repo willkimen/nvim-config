9e4c4f27a9c5b35840be85cf3c7724dc3d737727
Willkimen <willkimen.dev@proton.me>
2026-04-01 19:28:23 -0300

    refactor: Refactor treesitter attach code

--- lua/plugins/treesitter/nvim-treesitter.lua
+++ lua/plugins/treesitter/nvim-treesitter.lua
@@ -12,30 +12,35 @@ return  { -- Highlight, edit, and navigate code
       'query', 'vim', 'vimdoc', 'dockerfile',
       'go', 'python', 'javascript', 'typescript', 'tsx'
     }
-    -- No repo do kickstart estava como require('nvim-treesitter').install(parsers), mas o quando eu
-    -- iniciava o neovim, gerava o erro: attempt to call field 'install' (a nil value)
-    -- Então troquei pela forma abaixo e funcinou. Foi o chatgpt quem indicou
-      require('nvim-treesitter').install(parsers)
-      vim.api.nvim_create_autocmd('FileType', {
-        callback = function(args)
-          local buf, filetype = args.buf, args.match
+    require('nvim-treesitter').install(parsers)
 
-          local language = vim.treesitter.language.get_lang(filetype)
-          if not language then return end
+    ---@param buf integer
+    ---@param language string
+    local function treesitter_try_attach(buf, language)
+      -- check if parser exists and load it
+      if not vim.treesitter.language.add(language) then return end
+      -- enables syntax highlighting and other treesitter features
+      vim.treesitter.start(buf, language)
 
-          -- check if parser exists and load it
-          if not vim.treesitter.language.add(language) then return end
-          -- enables syntax highlighting and other treesitter features
-          vim.treesitter.start(buf, language)
+      -- enables treesitter based folds
+      -- for more info on folds see `:help folds`
+      -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+      -- vim.wo.foldmethod = 'expr'
 
-          -- enables treesitter based folds
-          -- for more info on folds see `:help folds`
-          -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-          -- vim.wo.foldmethod = 'expr'
+      -- enables treesitter based indentation
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end
 
-          -- enables treesitter based indentation
-          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-        end,
-      })
-    end,
-  }
+    local available_parsers = require('nvim-treesitter').get_available()
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function(args)
+        local buf, filetype = args.buf, args.match
+
+        local language = vim.treesitter.language.get_lang(filetype)
+        if not language then return end
+
+        treesitter_try_attach(buf, language)
+      end,
+    })
+  end,
+}
