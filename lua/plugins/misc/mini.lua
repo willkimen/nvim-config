d1dda05f5d6cf1ef74191f8a0fda07f189eb3d24
Willkimen <willkimen.dev@proton.me>
2026-04-23 14:40:06 -0300

    Fix mini.ai example

--- lua/plugins/misc/mini.lua
+++ lua/plugins/misc/mini.lua
@@ -5,7 +5,7 @@ return { -- Collection of various small independent plugins/modules
     --
     -- Examples:
     --  - va)  - [V]isually select [A]round [)]paren
-    --  - yinq - [Y]ank [I]nside [I]next [Q]uote
+    --  - yiinq - [Y]ank [I]nside [I]+1 [Q]uote
     --  - ci'  - [C]hange [I]nside [']quote
     require('mini.ai').setup {
       -- NOTE: Avoid conflicts with the built-in incremental selection mappings on Neovim>=0.12 (see `:help treesitter-incremental-selection`)
